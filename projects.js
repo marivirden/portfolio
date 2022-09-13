7ae7eda4544e810343da79db6890a41e39323915
marivirden <97749369+marivirden@users.noreply.github.com>
2022-09-13 14:32:19 -0500

    Additional information and JS to projects

---
+++ projects.js
@@ -0,0 +1,6 @@
+function addEventListenerToRedirect(buttonId, projectUrl) {}
+
+const buttonB = document.querySelector(".weather-button");
+buttonB.addEventListener("click", function () {
+  window.location = "https://unrivaled-frangollo-6081d2.netlify.app/";
+});
